ac889408728ece7a18b6916f300b3305e137064f
Alberto Gonzalez <albertoglezb@hotmail.com>
2025-08-11 13:29:03 -0600

    Changes for the class: 'Creating NodeJS + Express Setup'.

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = 3001;
+
+app.get("/", (req, res) => {
+  res.send("Express application");
+});
+
+app.listen(port, () => {
+  console.log(`Server running at: ${port}`);
+});
