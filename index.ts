4420f58e733710955ab2b5f484ab6ed703477b40
Alberto Gonzalez <albertoglezb@hotmail.com>
2025-08-12 09:30:55 -0600

    Changes for the class: 'Understanding the need for dependency injection'

--- index.ts
+++ index.ts
@@ -1,11 +1,24 @@
 import express from "express";
 import type { Request, Response, Express } from "express";
+import { Page } from "./src/page.js";
+import { User } from "./src/user.js";
+import { Post } from "./src/post.js";
 
 const app: Express = express();
 const port = 3001;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express application");
+});
+
+app.post("/create-post", (req: Request, res: Response) => {
+  const post = new Post("New Post", "Post Content", new User("John"));
+  res.send("Post created");
+});
+
+app.post("/create-page", (req: Request, res: Response) => {
+  let post = new Page("http://page.com", new User("John"));
+  res.send("Page created");
 });
 
 app.listen(port, () => {
---
+++ src/post.ts
@@ -0,0 +1,5 @@
+import type { User } from "./user.js";
+
+export class Post {
+  constructor(title: string, content: string, user: User) {}
+}
