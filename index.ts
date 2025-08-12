56a8e2df92b728d8b131d585a6aa614ea957a4c9
Alberto Gonzalez <albertoglezb@hotmail.com>
2025-08-12 07:23:00 -0600

    Changes for the class: 'Auto-reloading During Development'

--- index.ts
+++ index.ts
@@ -9,5 +9,5 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running at: ${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
